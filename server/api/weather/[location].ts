0bfad2149461aa4d66749c34ea963a74db06f810
Prateek Aghi <prateekaghi42@gmail.com>
2022-12-17 12:05:35 +0530

    consoles removed

--- server/api/weather/[location].ts
+++ server/api/weather/[location].ts
@@ -1,7 +1,6 @@
 export default defineEventHandler(async (event) => {
-  console.log(event.context);
   const location = event.context.params.location;
-  console.log("this is the location", location);
+
   let errorBody;
   const uri = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=8c31ec4140dcf28aee6388572bcebab2`;
   const weather = await $fetch(uri).catch((error) => {
